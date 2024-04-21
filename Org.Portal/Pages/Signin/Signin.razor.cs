de458607b6462bd7976c88f8e1d2f98b955b2bf6
ibtihal07 <ibtihalberdjouh@gmail.com>
2024-04-22 01:32:12 +0200

    Signin Page

---
+++ Org.Portal/Pages/Signin/Signin.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyProject.Pages
+{
+    public partial class Login
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        private void HandleLoginSubmit()
+        {
+            if (Username == "user" && Password == "password")
+            {
+                NavigationManager.NavigateTo("/Index");
+            }
+            else
+            {
+                ErrorMessage = "Invalid username or password.";
+            }
+        }
+    }
+}
